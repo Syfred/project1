27a3d5f2c961ebecfa115b7736becc36a80bcd4d
syfred <syfretngole@gmail.com>
2025-03-24 15:34:54 +0100

    added application.sh

---
+++ application.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Prompt the user for a file name
+read -p "Enter the file name: " FILE_NAME
+
+#Check if the file exists
+if [ -f "$FILE_NAME" ]; then
+  echo "File '$FILE_NAME' already exists."
+else
+  echo "File '$FILE_NAME' does not exist. Create file."
+  touch "$FILE_NAME"
+  echo "File '$FILE_NAME' has been created."
+fi
